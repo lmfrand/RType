46cffbe0b860ce44de1606854665a4c5d683aa88
LuisMiguel <LuisMiguel@DESKTOP-KRN8NS5>
2021-08-13 13:17:43 +0200

    Inicio proyecto

---
+++ src/main/java/RType/RType.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RType;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author LuisMiguel
+ */
+public class RType {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        JOptionPane.showMessageDialog(null, "Bienvenido a RType. ¿Desea iniciar el juego?");
+        
+        
+    }
+    
+}
